f624966312975b64b7d65ea142e3e76462795773
justifeany <ifeanyionuora79@gmail.com>
2025-12-17 16:16:48 +0100

    update on 3rd security group

--- ec2-demo.tf
+++ ec2-demo.tf
@@ -73,6 +73,41 @@ resource "aws_security_group" "server-sg2" {
   }
 }
 
+resource "aws_security_group" "server-sg3" {
+  name = "server-sg"
+  description = "Security group for test servers"
+  tags = {
+        Name = "test-sg"
+  }
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "database-servers" {
   ami = "ami-0ecb62995f68bb549"
   instance_type = "t2.micro"
